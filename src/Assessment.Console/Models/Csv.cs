de37c497f140a0979f30c3389aa607d09e8fb57f
Davide Corrain <d.corrain@gmail.com>
2023-10-30 14:40:32 +0100

    Rich model extra code without records

--- src/Assessment.Console/Models/Csv.cs
+++ src/Assessment.Console/Models/Csv.cs
@@ -10,4 +10,30 @@ public class Csv
         GivenName = givenName ?? string.Empty;
         FamilyName = familyName ?? string.Empty;
     }
+
+    public bool Equals(Csv? other)
+    {
+        if (ReferenceEquals(other, null)) return false;
+        if (ReferenceEquals(other, this)) return true;
+        return string.Equals(GivenName, other.GivenName, StringComparison.InvariantCultureIgnoreCase)
+            && string.Equals(FamilyName, other.FamilyName, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(obj, null)) return false;
+        if (ReferenceEquals(obj, this)) return true;
+        return obj.GetType() == this.GetType()
+            && Equals((Csv)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        HashCode hashCode = new();
+        hashCode.Add(GivenName, StringComparer.InvariantCultureIgnoreCase);
+        hashCode.Add(FamilyName, StringComparer.InvariantCultureIgnoreCase);
+
+        return hashCode.ToHashCode();
+    }    
 }
