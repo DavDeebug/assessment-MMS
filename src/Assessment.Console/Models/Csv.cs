16b459fef2ca6cb66a2e66ca35464cde253f731b
Davide Corrain <d.corrain@gmail.com>
2023-10-31 11:58:54 +0100

    Implement the rich model using records.

--- src/Assessment.Console/Models/Csv.cs
+++ src/Assessment.Console/Models/Csv.cs
@@ -1,39 +1,7 @@
 namespace Assessment.Console.Models;
 
-public class Csv
+public record Csv(string? GivenName, string? FamilyName)
 {
-    public string? GivenName { get; }
-    public string? FamilyName { get; }
-
-    public Csv(string? givenName, string? familyName)
-    {
-        GivenName = givenName ?? string.Empty;
-        FamilyName = familyName ?? string.Empty;
-    }
-
-    public bool Equals(Csv? other)
-    {
-        if (ReferenceEquals(other, null)) return false;
-        if (ReferenceEquals(other, this)) return true;
-        return string.Equals(GivenName, other.GivenName, StringComparison.InvariantCultureIgnoreCase)
-            && string.Equals(FamilyName, other.FamilyName, StringComparison.InvariantCultureIgnoreCase);
-    }
-
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(obj, null)) return false;
-        if (ReferenceEquals(obj, this)) return true;
-        return obj.GetType() == this.GetType()
-            && Equals((Csv)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        HashCode hashCode = new();
-        hashCode.Add(GivenName, StringComparer.InvariantCultureIgnoreCase);
-        hashCode.Add(FamilyName, StringComparer.InvariantCultureIgnoreCase);
-
-        return hashCode.ToHashCode();
-    }    
+    public string GivenName { get; init; } = GivenName ?? string.Empty;
+    public string FamilyName { get; init; } = FamilyName ?? string.Empty;
 }
